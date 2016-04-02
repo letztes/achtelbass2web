33e98c55a7377287d690ace93b0f5e83d3b7867c
Artur Spengler <letztes@gmail.com>
2016-04-02 22:26:43 +0200

    Add a footer template

---
+++ imprint/index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+print "content-type:text/html\n\n"
+
+import cgi
+import cgitb
+#cgitb.enable()  # for troubleshooting
+from jinja2 import Template, Environment, PackageLoader
+
+env  = Environment(loader=PackageLoader('achtelbass', 'templates'))
+template = env.get_template('imprint.html')
+context  = {}
+
+form = cgi.FieldStorage()
+
+def index():
+
+    print template.render(context)
+
+index()
