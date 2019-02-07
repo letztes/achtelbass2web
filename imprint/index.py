00894802a577f40712b933f682f38252f8378f0d
Artur Spengler <artur.spengler@gmail.com>
2019-02-07 21:52:01 +0100

    Comment out cgi debugging
    Fixes #28

--- imprint/index.py
+++ imprint/index.py
@@ -5,7 +5,7 @@ print("content-type:text/html\n\n")
 
 import cgi
 import cgitb
-cgitb.enable()  # for troubleshooting
+#cgitb.enable()  # for troubleshooting
 from jinja2 import Template, Environment, PackageLoader
 
 env  = Environment(loader=PackageLoader('achtelbass', 'templates'))
